b0c8c79c6956d4bab122537de130c6c58042412a
Sammy Mureithi <sammymureithi20@gmail.com>
2022-10-12 04:11:22 +0300

    Scroll view

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import InputField from './Components/InputField';
+import ScrollViewComponent1 from './Components/ScrollView';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Sammy You can Do this</Text>
       <StatusBar style="auto" />
-      <InputField text={"Happy Customer Day"}/>
-    </View>
+      <InputField text={"Happy Customer Day"} />
+      <ScrollViewComponent1 />
+      <InputField text={"Happy Customer Day"} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
