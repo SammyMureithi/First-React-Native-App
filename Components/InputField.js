435fadd6758fe1424450147057f213a6933cfcaa
Sammy Mureithi <sammymureithi20@gmail.com>
2022-10-12 03:38:28 +0300

    Styles

--- Components/InputField.js
+++ Components/InputField.js
@@ -11,13 +11,27 @@ function InputField( { text } ) {
         setCunter( prevValue => prevValue + 1 );
         console.log("Clicked")
     }
+  
     return (
-        <>
-            <Text onPress={handleClick}>Hello Again {customerName} ,{text} points { counter}</Text>
- 
-      </>
+        <View >
+            <Text onPress={handleClick} style={styles.Text1} >Hello Again {customerName} ,{text} points {counter}</Text>
+            <Text  style={styles.green}>Am Green Color</Text>
+      </View>
       
-  )
+    )
+    
 }
+const styles = StyleSheet.create( {
+    Text1: {
+        color: 'red',  
+        fontWeight: 'bold',  
+        fontSize: 30, 
+    },
+    green: {
+        color: "green",
+        textAlign: "center",
+        marginTop:20
+    }
+})
 
 export default InputField
